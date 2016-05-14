4662fda80515dc1fecec2800ef28da9172e2b2b3
cyrilsource <cyrilbron78@gmail.com>
2016-05-14 08:27:27 +0200

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,10 +1,10 @@
 <?php 
 
 $config = array(
-	'host'		=> '127.0.0.1',
-	'username'	=> 'root',
-	'password'	=> 'florence',
-	'database'	=> 'blogCyril'
+	'host'		=> '',
+	'username'	=> '',
+	'password'	=> '',
+	'database'	=> ''
 
 
 
